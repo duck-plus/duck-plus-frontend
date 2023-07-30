4c783af9f69a6919088df81c026a47f9477532d4
taekwon.yoo <takun617@gmail.com>
2023-07-30 16:26:34 +0900

    [chore] added import alias '@'
    - '@' for '/src' applied.

--- craco.config.js
+++ craco.config.js
@@ -1,8 +1,15 @@
+const path = require("path");
+
 module.exports = {
   babel: {
     plugins: [
       process.env.NODE_ENV === "development" &&
         "babel-plugin-styled-components",
     ].filter(Boolean),
+  },
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 };
