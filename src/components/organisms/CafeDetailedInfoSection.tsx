637f4c69b3db25306d65928733ea987bcaf7f2ac
taekwon.yoo <takun617@gmail.com>
2023-08-12 23:20:50 +0900

    카페 상세 > 상세 정보 일부 구현

---
+++ src/components/organisms/CafeDetailedInfoSection.tsx
@@ -0,0 +1,268 @@
+import React from "react";
+import { Cafe } from "@/services/gql-outputs/graphql";
+import useHorizontalRatio, { hScalePx } from "@/hooks/useHorizontalRatio";
+import styled, { useTheme } from "styled-components";
+import AppTopBar from "./AppTopBar";
+import { ReactComponent as DotSVGR } from "@/assets/svgr/ic/dot.svg";
+import { ReactComponent as ZoomInSVGR } from "@/assets/svgr/ic/zoom-in.svg";
+
+const CafeDetailedInfoSectionFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const DetailInfoNavBar = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  padding: 0 ${hScalePx(20)};
+  gap: ${hScalePx(20)};
+  position: sticky;
+  top: calc(${AppTopBar.CSSAppTopBarHeight} - 1px);
+  display: flex;
+  height: ${hScalePx(40)};
+  z-index: 100;
+`;
+
+const DetailInfoNavBarItem = styled.div<{ selected: boolean }>`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  ${({ theme }) => theme.fontFaces["body2/12-Medium"]};
+  color: ${({ theme, selected }) =>
+    selected ? theme.colors.gray900 : theme.colors.gray500};
+  cursor: pointer;
+`;
+
+const DetailedInfo = styled.div`
+  padding: ${hScalePx(24)} ${hScalePx(20)};
+  display: flex;
+  flex-direction: column;
+  gap: ${hScalePx(12)};
+  white-space: pre-wrap;
+`;
+
+const HorSep = styled.div`
+  margin: 0 ${hScalePx(20)};
+  border-bottom: ${hScalePx(1)} solid ${({ theme }) => theme.colors.gray100};
+`;
+
+const Title = styled.div`
+  ${({ theme }) => theme.fontFaces["body2/12-Regular"]};
+  color: ${({ theme }) => theme.colors.gray500};
+  height: ${hScalePx(20)};
+`;
+
+const Info = styled.div`
+  ${({ theme }) => theme.fontFaces["body2/12-Regular"]};
+  color: ${({ theme }) => theme.colors.gray900};
+  display: flex;
+  align-items: center;
+`;
+
+const InfoList = styled.div`
+  ${({ theme }) => theme.fontFaces["body2/12-Regular"]};
+  display: flex;
+  flex-direction: column;
+`;
+
+const InfoListItemFrame = styled(Info)`
+  display: flex;
+  flex-direction: row;
+`;
+
+const InfoTags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: ${hScalePx(4)};
+  row-gap: ${hScalePx(6)};
+`;
+
+const InfoTag = styled.div`
+  ${({ theme }) => theme.fontFaces["body2/12-Regular"]};
+  color: ${({ theme }) => theme.colors.gray900};
+  padding: 0 ${hScalePx(12)};
+  min-height: ${hScalePx(28)};
+  background-color: ${({ theme }) => theme.colors.gray100};
+  border-radius: ${hScalePx(100)};
+  display: flex;
+  align-items: center;
+`;
+
+const MenuInfo = styled.div`
+  ${({ theme }) => theme.fontFaces["caption/10-Regular"]};
+  color: ${({ theme }) => theme.colors.gray500};
+  display: flex;
+  flex-direction: column;
+  gap: ${hScalePx(6)};
+`;
+
+const MenuImageFrame = styled.div`
+  position: relative;
+`;
+
+const MenuDisclaimer = styled.div`
+  ${({ theme }) => theme.fontFaces["caption/10-Regular"]};
+  color: ${({ theme }) => theme.colors.gray500};
+`;
+
+const MenuImage = styled.img`
+  width: 100%;
+  object-fit: contain;
+  cursor: pointer;
+`;
+
+const ZoomInIcon = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.4);
+  width: ${hScalePx(32)};
+  height: ${hScalePx(32)};
+  padding: ${hScalePx(4)};
+  cursor: pointer;
+`;
+
+const detailInfoItems = ["카페정보", "특전안내", "메뉴", "지도"] as const;
+
+const splitToArray = (s: string | undefined | null) =>
+  s && s.split(/\r\n|\r|\n/).map((s) => s.trim());
+
+const InfoListItem = ({ children }: React.PropsWithChildren) => {
+  const hr = useHorizontalRatio();
+  const theme = useTheme();
+
+  return (
+    <InfoListItemFrame>
+      <DotSVGR width={hr * 16} height={hr * 16} fill={theme.colors.gray900} />
+      {children}
+    </InfoListItemFrame>
+  );
+};
+
+interface IProps {
+  cafe: Cafe | null | undefined;
+}
+
+/** 카페 상세 페이지 > 상세 정보들 */
+const CafeDetailedInfoSection = ({ cafe }: IProps) => {
+  const theme = useTheme();
+
+  const facilityList = splitToArray(cafe?.specialBenefit);
+
+  const specialBenefitList = splitToArray(cafe?.specialBenefit);
+
+  const menuImage = cafe?.imageFileList?.find((s) => s?.category === "MENU");
+
+  const handleMenuImageClick = (url: string) => {
+    alert("TODO: implement Gallery (확대기능)");
+  };
+
+  return !cafe ? null : (
+    <CafeDetailedInfoSectionFrame>
+      <HorSep
+        style={{
+          margin: "0",
+          borderBottom: `${hScalePx(8)} solid ${theme.colors.gray50}`,
+        }}
+      />
+      <DetailInfoNavBar>
+        {detailInfoItems.map((item) => (
+          // TODO) inView 활용해서, selected 컨트롤
+          <DetailInfoNavBarItem selected>{item}</DetailInfoNavBarItem>
+        ))}
+      </DetailInfoNavBar>
+      <HorSep style={{ margin: "0" }} />
+
+      {/* 카페정보 */}
+      <DetailedInfo>
+        <Title>카페정보</Title>
+        <Info>{cafe.detailedInfo}</Info>
+      </DetailedInfo>
+      <HorSep />
+
+      {/* 시설안내 */}
+      {!facilityList ? null : (
+        <>
+          <DetailedInfo>
+            <Title>시설안내</Title>
+            <InfoList>
+              {facilityList.map((facility) => (
+                <InfoListItem>{facility}</InfoListItem>
+              ))}
+            </InfoList>
+          </DetailedInfo>
+          <HorSep />
+        </>
+      )}
+
+      {/* 특이사항 */}
+      {!cafe?.remarkList ? null : (
+        <>
+          <DetailedInfo>
+            <Title>특이사항</Title>
+            <InfoTags>
+              {cafe?.remarkList.map((benefit) => (
+                <InfoTag>{benefit}</InfoTag>
+              ))}
+            </InfoTags>
+          </DetailedInfo>
+          <HorSep />
+        </>
+      )}
+
+      {/* 지원 특전 목록 */}
+      <DetailedInfo>
+        <Title>지원특전목록</Title>
+        <InfoList>
+          {!specialBenefitList ? (
+            <InfoListItem>정보 없음</InfoListItem>
+          ) : (
+            specialBenefitList.map((benefit) => (
+              <InfoListItem>{benefit}</InfoListItem>
+            ))
+          )}
+        </InfoList>
+      </DetailedInfo>
+      <HorSep />
+
+      {/* 음료 및 디저트 메뉴판 */}
+      {!menuImage ? null : (
+        <>
+          <DetailedInfo>
+            <Title>음료 및 디저트 메뉴판</Title>
+            <MenuInfo>
+              {/* 이미지 */}
+              <MenuImageFrame>
+                <MenuImage
+                  src={menuImage.url}
+                  onClick={() => handleMenuImageClick(menuImage.url)}
+                />
+                {/* 돋보기 */}
+                <ZoomInIcon>
+                  <ZoomInSVGR width="100%" height="100%" />
+                </ZoomInIcon>
+              </MenuImageFrame>
+              {/* 면책 조항 */}
+              <MenuDisclaimer>
+                *이벤트 메뉴와는 상이하며 구성 및 금액은 문의를 통해
+                확인해주세요
+              </MenuDisclaimer>
+            </MenuInfo>
+          </DetailedInfo>
+          <HorSep />
+        </>
+      )}
+
+      {/* 상세위치 */}
+      <DetailedInfo>
+        <Title>상세위치</Title>
+        <Info>🚧🚧🚧</Info>
+      </DetailedInfo>
+
+      {/* TODO) TBD */}
+      {/* ?? */}
+      {/* 환불규정 */}
+    </CafeDetailedInfoSectionFrame>
+  );
+};
+
+export default CafeDetailedInfoSection;
--- src/components/pages/CafeDetailPage.tsx
+++ src/components/pages/CafeDetailPage.tsx
@@ -1,25 +1,19 @@
 import React, { useEffect, useState } from "react";
 import PageFrame from "../atoms/PageFrame";
 import AppTopBar from "../organisms/AppTopBar";
-import styled, { useTheme } from "styled-components";
+import styled from "styled-components";
 import EmblaCarousel from "../organisms/EmblaCarousel";
-import useHorizontalRatio, { hScalePx } from "@/hooks/useHorizontalRatio";
+import { hScalePx } from "@/hooks/useHorizontalRatio";
 import {
-  Day,
   useGetCafeListQuery,
   useGetCafeQuery,
 } from "@/services/gql-outputs/graphql";
 import { useTypedSearchParams } from "react-router-typesafe-routes/dom";
 import { ROUTES } from "@/router";
 import useEmblaCarousel from "embla-carousel-react";
-import { ReactComponent as LocationSVGR } from "@/assets/svgr/ic/location.svg";
-import { ReactComponent as TimeSVGR } from "@/assets/svgr/ic/time.svg";
-import { ReactComponent as InstagramSVGR } from "@/assets/svgr/ic/instagram.svg";
-// FIXME) 카카오 svg 나오면 교체 필요
-import { ReactComponent as KakaoSVGR } from "@/assets/svgr/ic/instagram.svg";
-// FIXME) 트위터 svg 나오면 교체 필요
-import { ReactComponent as TwitterSVGR } from "@/assets/svgr/ic/instagram.svg";
 import isNotNull from "@/utils/isNotNull";
+import CafeBreifInfoSection from "../organisms/CafeBriefInfoSection";
+import CafeDetailInfoSection from "../organisms/CafeDetailedInfoSection";
 
 // carousel
 const CafeCarousel = styled(EmblaCarousel.Embla)`
@@ -60,68 +54,6 @@ const CarouselDot = styled.div<{ selected: boolean }>`
   cursor: pointer;
 `;
 
-const CafeDesc1 = styled.div`
-  padding: ${hScalePx(16)} ${hScalePx(20)} ${hScalePx(12)} ${hScalePx(20)};
-  display: flex;
-  flex-direction: column;
-  gap: ${hScalePx(2)};
-`;
-const CafeDesc2 = styled.div`
-  padding: ${hScalePx(16)} ${hScalePx(20)};
-  display: flex;
-  flex-direction: column;
-  gap: ${hScalePx(6)};
-`;
-const Desc2Item = styled.div`
-  ${({ theme }) => theme.fontFaces["body2/12-Regular"]};
-  color: ${({ theme }) => theme.colors.gray800};
-  display: flex;
-  height: ${hScalePx(20)};
-  align-items: center;
-  gap: ${hScalePx(6)};
-`;
-
-const Name = styled.div`
-  ${({ theme }) => theme.fontFaces["title2/16-Regular"]};
-  color: ${({ theme }) => theme.colors.gray900};
-`;
-const BriefInfo = styled.div`
-  ${({ theme }) => theme.fontFaces["body2/12-Regular"]};
-  color: ${({ theme }) => theme.colors.gray800};
-`;
-
-const HorSep = styled.div`
-  width: ${hScalePx(320)};
-  margin: 0 auto;
-  border-bottom: ${hScalePx(1)} solid ${({ theme }) => theme.colors.gray100};
-`;
-
-const SNSChannelName = styled.div`
-  text-decoration: underline;
-  cursor: pointer;
-`;
-
-const dayToLocaleStringMap: Record<Day, string> = {
-  FRI: "금",
-  MON: "월",
-  SAT: "토",
-  SUN: "일",
-  THU: "목",
-  TUE: "화",
-  WED: "수",
-} as const;
-
-const NoHolidayBadge = styled.div`
-  ${({ theme }) => theme.fontFaces["overline/9-SemiBold"]};
-  color: ${({ theme }) => theme.colors.gray700};
-  background-color: ${({ theme }) => theme.colors.gray100};
-  border-radius: ${hScalePx(2)};
-  padding: 0 ${hScalePx(4)};
-  height: ${hScalePx(14)};
-  display: flex;
-  align-items: center;
-`;
-
 const CafeDetailPage = () => {
   const [{ code }] = useTypedSearchParams(ROUTES.CAFE.DETAILS);
 
@@ -157,13 +89,14 @@ const CafeDetailPage = () => {
     });
   }, [emblaApi]);
 
-  const hr = useHorizontalRatio();
-  const theme = useTheme();
-
   if (!code) {
     alert("잘못된 접근입니다.");
     window.location.href = "/";
   }
+
+  const landscapeImages = cafe?.imageFileList
+    .filter(isNotNull)
+    .filter(({ category }) => category === "LANDSCAPE");
 
   return (
     <PageFrame>
@@ -171,20 +104,20 @@ const CafeDetailPage = () => {
       {/* Cafe Image Carousel */}
       <CafeCarousel ref={emblaRef}>
         <Container>
-          {cafe?.imageFileList.map((img) =>
+          {landscapeImages?.map((img) =>
             img ? (
               <Slide>
-                <img alt={cafe.name} src={img.url} />
+                <img alt={cafe?.name} src={img.url} />
               </Slide>
             ) : null
           )}
         </Container>
         {/* 캐로샐 탐색용 점들 */}
         <CarouselDots>
-          {cafe?.imageFileList.map((_, idx) => {
+          {landscapeImages?.map((_, idx) => {
             return (
               <CarouselDot
-                key={`${cafe.name}_${idx}`}
+                key={`${cafe?.name || ""}_${idx}`}
                 selected={idx === selectedImageIdx}
                 onClick={(e) => handleDotClick(e, idx)}
               />
@@ -192,75 +125,8 @@ const CafeDetailPage = () => {
           })}
         </CarouselDots>
       </CafeCarousel>
-      {cafe ? (
-        <>
-          {/* Cafe Description */}
-          <CafeDesc1>
-            <Name>{cafe.name}</Name>
-            <BriefInfo>{cafe.briefInfo}</BriefInfo>
-          </CafeDesc1>
-          <HorSep></HorSep>
-          <CafeDesc2>
-            <Desc2Item>
-              <LocationSVGR
-                width={hr * 16}
-                height={hr * 16}
-                fill={theme.colors.gray800}
-              />
-              {cafe.address.briefAddress}
-            </Desc2Item>
-            {cafe.snsList
-              .filter(isNotNull)
-              .map(({ type, channelName, url }) => {
-                const SNSIcon =
-                  type === "KAKAO"
-                    ? KakaoSVGR
-                    : type === "INSTAGRAM"
-                    ? InstagramSVGR
-                    : TwitterSVGR;
-
-                return (
-                  <Desc2Item key={`${channelName}_${url}`}>
-                    <SNSIcon
-                      width={hr * 16}
-                      height={hr * 16}
-                      fill={theme.colors.gray800}
-                    />
-                    <SNSChannelName onClick={() => window.open(url)}>
-                      {channelName}
-                    </SNSChannelName>
-                  </Desc2Item>
-                );
-              })}
-            <Desc2Item>
-              <TimeSVGR
-                width={hr * 16}
-                height={hr * 16}
-                fill={theme.colors.gray800}
-              />
-              {/* 요일 */}
-              {cafe.businessHour.businessDayList.length === 7
-                ? "매일"
-                : cafe.businessHour.businessDayList
-                    .filter(isNotNull)
-                    .reduce(
-                      (prev, day, idx) =>
-                        `${prev}${idx > 0 ? " " : ""}${
-                          dayToLocaleStringMap[day]
-                        }`,
-                      ""
-                    ) + " "}
-              {/* 시간 */}
-              {cafe.businessHour.openingTime}~{cafe.businessHour.closingTime}
-              {/* 휴일에도 운영 하면 휴무없음 딱지*/}
-              {cafe.businessHour.workingOnHoliday ? (
-                <NoHolidayBadge>휴무없음</NoHolidayBadge>
-              ) : null}
-            </Desc2Item>
-          </CafeDesc2>
-        </>
-      ) : null}
-      {/* TODO) show CafeDetails.. */}
+      <CafeBreifInfoSection cafe={cafe} />
+      <CafeDetailInfoSection cafe={cafe} />
     </PageFrame>
   );
 };
