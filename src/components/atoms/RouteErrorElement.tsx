a00b005dbc02479490dd2c7a5d75964b5944c3af
taekwon.yoo <takun617@gmail.com>
2023-08-13 17:31:49 +0900

    RouteErrorElement 고도화: 배경색 넣음. 크기 키움. 에러메시지 내용을 뿌리도록 변경

--- src/components/atoms/RouteErrorElement.tsx
+++ src/components/atoms/RouteErrorElement.tsx
@@ -1,15 +1,37 @@
-import { useLocation, useRouteError } from "react-router-dom";
+import {
+  isRouteErrorResponse,
+  useLocation,
+  useRouteError,
+} from "react-router-dom";
+import { styled } from "styled-components";
+
+const RouteErrorFrame = styled.div`
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: white;
+  font-size: 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-weight: 700;
+  color: red;
+`;
 
 // 라우팅 / navigate 등 client-side routing 중 render 오류 발생 시 노출할 fallback UI
 const RouteErrorElement = () => {
-  const routerError = useRouteError();
+  const routerError = useRouteError() as any;
   const location = useLocation();
   console.error(routerError);
   return (
-    <div>
-      Error occured while routing: {window.location.host}
-      {location.pathname}
-    </div>
+    <RouteErrorFrame>
+      {isRouteErrorResponse(routerError)
+        ? routerError.data
+        : `Error occured while routing: ${window.location.host} ${location.pathname}`}
+    </RouteErrorFrame>
   );
 };
 
