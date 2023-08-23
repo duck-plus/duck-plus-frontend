ada4105c2efb9d3ce99fd8256e7eb3ae5f877361
taekwon.yoo <takun617@gmail.com>
2023-08-24 02:22:13 +0900

    견적내기 로딩 화면, 최소 3초간 로딩 보여주도록 수정

--- src/components/pages/CalcResultPage.tsx
+++ src/components/pages/CalcResultPage.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import PageFrame from "../atoms/PageFrame";
-import AppTopBar from "../organisms/AppTopBar";
-import CafeList from "../organisms/CafeList";
-import { useTypedSearchParams } from "react-router-typesafe-routes/dom";
-import { ROUTES } from "@/router";
-import { styled } from "styled-components";
-import { hScalePx } from "@/hooks/useHorizontalRatio";
-import LoadingPage from "./LoadingPage";
+import React, { useEffect, useState } from 'react';
+import PageFrame from '../atoms/PageFrame';
+import AppTopBar from '../organisms/AppTopBar';
+import CafeList from '../organisms/CafeList';
+import { useTypedSearchParams } from 'react-router-typesafe-routes/dom';
+import { ROUTES } from '@/router';
+import { styled } from 'styled-components';
+import { hScalePx } from '@/hooks/useHorizontalRatio';
+import LoadingPage from './LoadingPage';
 
 const Padding = styled.div`
   height: ${hScalePx(12)};
@@ -17,14 +17,24 @@ const loadingMessage = `조금만 기다려주세요
 
 const CalcResultPage = () => {
   const [{ dailyCharge }] = useTypedSearchParams(ROUTES.CALC.RESULT);
+
+  const [showLoadingFaker, setShowLoadingFaker] = useState<boolean>(true);
+  useEffect(() => {
+    const timeoutHandle = setTimeout(() => {
+      setShowLoadingFaker(false);
+    }, 3e3);
+    return () => clearTimeout(timeoutHandle);
+  }, []);
+
   return (
-    <PageFrame>
-      <React.Suspense fallback={<LoadingPage message={loadingMessage} />}>
+    <React.Suspense fallback={<LoadingPage key="loading" message={loadingMessage} />}>
+      <PageFrame>
         <AppTopBar.Center>견적내기</AppTopBar.Center>
         <Padding />
         <CafeList filter={{ dailyCharge }}></CafeList>
-      </React.Suspense>
-    </PageFrame>
+      </PageFrame>
+      {showLoadingFaker ? <LoadingPage key="loading" message={loadingMessage} /> : null}
+    </React.Suspense>
   );
 };
 
