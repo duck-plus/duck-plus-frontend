6f7b0db60a6892989d0a45b4d62587a35449661a
taekwon.yoo <takun617@gmail.com>
2023-08-23 18:11:23 +0900

    매일 -> 매일(공백)

--- src/components/organisms/CafeBriefInfoSection.tsx
+++ src/components/organisms/CafeBriefInfoSection.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Day, useGetCafeQuery } from "@/services/gql-outputs/graphql";
-import useHorizontalRatio, { hScalePx } from "@/hooks/useHorizontalRatio";
-import { ReactComponent as LocationSVGR } from "@/assets/svgr/ic/location.svg";
-import { ReactComponent as TimeSVGR } from "@/assets/svgr/ic/time.svg";
-import { ReactComponent as InstagramSVGR } from "@/assets/svgr/ic/instagram.svg";
-import { ReactComponent as KakaoSVGR } from "@/assets/svgr/ic/kakao.svg";
-import { ReactComponent as TwitterSVGR } from "@/assets/svgr/ic/twitter.svg";
-import styled, { useTheme } from "styled-components";
-import isNonNullable from "@/utils/isNonNullable";
-import openURL from "@/utils/openURL";
-import ga from "@/utils/ga";
+import React from 'react';
+import { Day, useGetCafeQuery } from '@/services/gql-outputs/graphql';
+import useHorizontalRatio, { hScalePx } from '@/hooks/useHorizontalRatio';
+import { ReactComponent as LocationSVGR } from '@/assets/svgr/ic/location.svg';
+import { ReactComponent as TimeSVGR } from '@/assets/svgr/ic/time.svg';
+import { ReactComponent as InstagramSVGR } from '@/assets/svgr/ic/instagram.svg';
+import { ReactComponent as KakaoSVGR } from '@/assets/svgr/ic/kakao.svg';
+import { ReactComponent as TwitterSVGR } from '@/assets/svgr/ic/twitter.svg';
+import styled, { useTheme } from 'styled-components';
+import isNonNullable from '@/utils/isNonNullable';
+import openURL from '@/utils/openURL';
+import ga from '@/utils/ga';
 
 const BreifIntro = styled.div`
   padding: ${hScalePx(16)} ${hScalePx(20)} ${hScalePx(12)} ${hScalePx(20)};
@@ -24,7 +24,7 @@ const CafeBriefInfo = styled.div`
   gap: ${hScalePx(6)};
 `;
 const DetailItem = styled.div`
-  ${({ theme }) => theme.fontFaces["body2/12-Regular"]};
+  ${({ theme }) => theme.fontFaces['body2/12-Regular']};
   color: ${({ theme }) => theme.colors.gray800};
   display: flex;
   height: ${hScalePx(20)};
@@ -33,11 +33,11 @@ const DetailItem = styled.div`
 `;
 
 const Name = styled.div`
-  ${({ theme }) => theme.fontFaces["title2/16-Regular"]};
+  ${({ theme }) => theme.fontFaces['title2/16-Regular']};
   color: ${({ theme }) => theme.colors.gray900};
 `;
 const BriefIntro = styled.div`
-  ${({ theme }) => theme.fontFaces["body2/12-Regular"]};
+  ${({ theme }) => theme.fontFaces['body2/12-Regular']};
   color: ${({ theme }) => theme.colors.gray800};
 `;
 
@@ -53,17 +53,17 @@ const SNSChannelName = styled.div`
 `;
 
 const dayToLocaleStringMap: Record<Day, string> = {
-  FRI: "금",
-  MON: "월",
-  SAT: "토",
-  SUN: "일",
-  THU: "목",
-  TUE: "화",
-  WED: "수",
+  FRI: '금',
+  MON: '월',
+  SAT: '토',
+  SUN: '일',
+  THU: '목',
+  TUE: '화',
+  WED: '수',
 } as const;
 
 const NoHolidayBadge = styled.div`
-  ${({ theme }) => theme.fontFaces["overline/9-SemiBold"]};
+  ${({ theme }) => theme.fontFaces['overline/9-SemiBold']};
   color: ${({ theme }) => theme.colors.gray700};
   background-color: ${({ theme }) => theme.colors.gray100};
   border-radius: ${hScalePx(2)};
@@ -86,7 +86,7 @@ const CafeBriefInfoSection = ({ cafeCode }: IProps) => {
     },
     {
       enabled: !!cafeCode,
-      select: (s) => s.cafe,
+      select: s => s.cafe,
     }
   );
 
@@ -100,70 +100,49 @@ const CafeBriefInfoSection = ({ cafeCode }: IProps) => {
       <CafeBriefInfo>
         {/* 간략 주소 */}
         <DetailItem>
-          <LocationSVGR
-            width={hr * 16}
-            height={hr * 16}
-            fill={theme.colors.gray800}
-          />
+          <LocationSVGR width={hr * 16} height={hr * 16} fill={theme.colors.gray800} />
           {cafe?.address.briefAddress}
         </DetailItem>
         {/* SNS */}
-        {cafe?.snsList
-          .filter(isNonNullable)
-          .map(({ type, channelName, url }) => {
-            const SNSIcon =
-              type === "KAKAO"
-                ? KakaoSVGR
-                : type === "INSTAGRAM"
-                ? InstagramSVGR
-                : TwitterSVGR;
+        {cafe?.snsList.filter(isNonNullable).map(({ type, channelName, url }) => {
+          const SNSIcon =
+            type === 'KAKAO' ? KakaoSVGR : type === 'INSTAGRAM' ? InstagramSVGR : TwitterSVGR;
 
-            return (
-              <DetailItem key={`${channelName}_${url}`}>
-                <SNSIcon
-                  width={hr * 16}
-                  height={hr * 16}
-                  fill={theme.colors.gray800}
-                />
-                <SNSChannelName
-                  onClick={() => {
-                    ga.send("sns_btn", {
-                      type,
-                      cafeName: cafe.name,
-                      channelName,
-                      url,
-                    });
-                    openURL(url);
-                  }}
-                >
-                  {channelName}
-                </SNSChannelName>
-              </DetailItem>
-            );
-          })}
+          return (
+            <DetailItem key={`${channelName}_${url}`}>
+              <SNSIcon width={hr * 16} height={hr * 16} fill={theme.colors.gray800} />
+              <SNSChannelName
+                onClick={() => {
+                  ga.send('sns_btn', {
+                    type,
+                    cafeName: cafe.name,
+                    channelName,
+                    url,
+                  });
+                  openURL(url);
+                }}
+              >
+                {channelName}
+              </SNSChannelName>
+            </DetailItem>
+          );
+        })}
         {/* 운영 시간 */}
         <DetailItem>
-          <TimeSVGR
-            width={hr * 16}
-            height={hr * 16}
-            fill={theme.colors.gray800}
-          />
+          <TimeSVGR width={hr * 16} height={hr * 16} fill={theme.colors.gray800} />
           {/* 요일 */}
           {cafe?.businessHour.businessDayList.length === 7
-            ? "매일"
+            ? '매일 '
             : cafe?.businessHour.businessDayList
                 .filter(isNonNullable)
                 .reduce(
-                  (prev, day, idx) =>
-                    `${prev}${idx > 0 ? " " : ""}${dayToLocaleStringMap[day]}`,
-                  ""
-                ) + " "}
+                  (prev, day, idx) => `${prev}${idx > 0 ? ' ' : ''}${dayToLocaleStringMap[day]}`,
+                  ''
+                ) + ' '}
           {/* 시간 */}
           {cafe?.businessHour.openingTime}~{cafe?.businessHour.closingTime}
           {/* 휴일에도 운영 하면 휴무없음 딱지*/}
-          {cafe?.businessHour.workingOnHoliday ? (
-            <NoHolidayBadge>휴무없음</NoHolidayBadge>
-          ) : null}
+          {cafe?.businessHour.workingOnHoliday ? <NoHolidayBadge>휴무없음</NoHolidayBadge> : null}
         </DetailItem>
       </CafeBriefInfo>
     </div>
