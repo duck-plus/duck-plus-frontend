6122a42d9f7767ca302747daacaaf5c04a8a922b
taekwon.yoo <takun617@gmail.com>
2023-08-24 19:29:35 +0900

    changed color of letterbox on desktop to #f4f4f4, changed desktop layout max-width: to 410px

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,17 @@
-import React, { Suspense, useEffect } from "react";
-import { Outlet, useLocation } from "react-router-dom";
-import LoadingPage from "./pages/LoadingPage";
-import { HorizontalRatioBoundary } from "../hooks/useHorizontalRatio";
-import { ThemeProvider } from "styled-components";
-import { theme } from "@/theme";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import ga from "@/utils/ga";
+import React, { Suspense, useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import LoadingPage from './pages/LoadingPage';
+import { HorizontalRatioBoundary } from '../hooks/useHorizontalRatio';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import ga from '@/utils/ga';
+import UAParser from 'ua-parser-js';
+
+const deviceType = new UAParser(navigator.userAgent).getDevice().type;
+const isNotMobile =
+  deviceType !== UAParser.DEVICE.WEARABLE && deviceType !== UAParser.DEVICE.MOBILE;
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,16 +24,22 @@ const queryClient = new QueryClient({
 const App = () => {
   const location = useLocation();
 
+  useEffect(() => {}, []);
+
   useEffect(() => {
-    ga.send("page_view", { path: location.pathname });
+    ga.send('page_view', { path: location.pathname });
   }, [location.pathname]);
 
   return (
     <HorizontalRatioBoundary
-      style={{
-        maxWidth: "768px",
-        margin: "0 auto",
-      }}
+      style={
+        isNotMobile
+          ? {
+              maxWidth: '410px',
+              margin: '0 auto',
+            }
+          : {}
+      }
     >
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
